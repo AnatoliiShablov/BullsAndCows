19635f3ba90acd73284149703e83b6984126ddf7
Anatolii Shablov <anatoliishablov@gmail.com>
2020-06-30 16:19:02 +0300

    ? faster filter

--- src/AI_hard_player.cpp
+++ src/AI_hard_player.cpp
@@ -1,6 +1,7 @@
 #include "AI_hard_player.hpp"
 
 #include <random>
+#include <iostream>
 
 namespace {
 void fill_with_recursion(std::vector<bool>& used, std::string& current_string,
@@ -18,6 +19,22 @@ void fill_with_recursion(std::vector<bool>& used, std::string& current_string,
         current_string.pop_back();
         used[ch] = false;
     }
+}
+
+template <typename ForwardIt, typename UnaryPredicate>
+ForwardIt inplace_filter(ForwardIt first, ForwardIt last, UnaryPredicate p) {
+    if (first == last)
+        return last;
+ 
+    ForwardIt result = first;
+    while (first != last) {
+        if (p(*first)) {
+            if(result!=first) {*result = std::move(*first); }
+            ++result;
+        }
+        ++first;
+    }
+    return result;
 }
 }  // namespace
 
@@ -38,18 +55,22 @@ AI_hard_player::AI_hard_player() : AI_hard_player{10, 4} {}
 
 std::string AI_hard_player::get_variant() {
     if (all.size() == current_variants.size()) {
-        last_asked = all.front();
+        last_asked = all[static_cast<size_t>(std::rand()) % all.size()];
     } else if (current_variants.size() == 1) {
         last_asked = current_variants.front();
     } else if (current_variants.size() == 0) {
         last_asked = "error";
     } else {
         size_t min = current_variants.size() + 1;
+        std::vector<std::string_view> tmp;
+        for(size_t i = 0; i < current_variants.size() && i < 100; ++i) {
+        	tmp.emplace_back(current_variants[static_cast<size_t>(std::rand()) % current_variants.size()]);
+        }
         for (std::string const& rhs : all) {
             std::vector<std::vector<size_t>> counter(
                 all.front().length() + 1, std::vector<size_t>(all.front().length() + 1, 0));
             size_t tmp_max = 0;
-            for (std::string const& lhs : current_variants) {
+            for (std::string_view lhs : tmp) {
                 helper.get_match(lhs, rhs);
                 if (++counter[helper.bulls()][helper.cows()] > tmp_max) {
                     tmp_max = counter[helper.bulls()][helper.cows()];
@@ -68,14 +89,11 @@ std::string AI_hard_player::get_variant() {
 }
 
 void AI_hard_player::answer(const bulls_cows& answer) {
-    std::vector<std::string> new_variants;
-    for (std::string const& lhs : current_variants) {
-        helper.get_match(lhs, last_asked);
-        if (helper == answer) {
-            new_variants.push_back(lhs);
-        }
-    }
-    current_variants = new_variants;
+	std::cout << "Before filter: " << current_variants.size() << std::endl;
+	current_variants.erase( inplace_filter(current_variants.begin(), current_variants.end(), [this, &answer](std::string const & str){
+        return helper.get_match(str, last_asked) == answer;
+	}), current_variants.end());
+    std::cout << "After  filter: " << current_variants.size() << std::endl;
 }
 
 std::string_view AI_hard_player::get_value() const noexcept {
